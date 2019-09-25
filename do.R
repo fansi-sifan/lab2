fd5cef80855486f94dedebd46d9a6ab743db4827
fansi-sifan <sifan1121@gmail.com>
2019-09-25 17:47:32 -0400

    add school data

--- do.R
+++ do.R
@@ -1,23 +1,28 @@
+source("func.R")
 # do
 
 # GET ALL
 
-get_all <- function(code, stco_code, name){
-  oow <- get_oow(code)
-  yoow <- get_yoow(code)
-  lww <- get_lww(code)
-  opp <- get_opp(code)
+get_all <- function(metrocode, peercode, stco_code, name){
+  oow <- get_oow(metrocode, peercode)
+  yoow <- get_yoow(metrocode, peercode)
+  lww <- get_lww(metrocode, peercode)
+  opp <- get_opp(metrocode, peercode)
   
   sbo <- get_sbo_m(stco_code)
+  school <- get_school(stco_code)
+  hiratio <- get_hiratio(metrocode, peercode)
   
-  hiratio <- get_hiratio(code)
-  
-  openxlsx::write.xlsx(c(oow, yoow, lww, opp, sbo, hiratio), file = paste0(name,".xlsx"))
+  openxlsx::write.xlsx(c(oow, yoow, lww, opp, sbo, school, hiratio), file = paste0(name,".xlsx"))
   
 }
 
-get_all(MetroDenver_cbsa, MetroDenvr_actual, "Metro Denver")
-get_all(GR_cbsa, GR_county, "Grand Rapids")
+# denver data
+get_all(MetroDenver_cbsa,Denverpeer_cbsa, MetroDenvr_actual, "Metro Denver")
+
+# GR data
+get_all(GR_cbsa,GRpeer_cbsa,GR_county, "Grand Rapids")
 
 
 df <- get_sbo_m(MetroDenvr_actual)
+
