c46ecd6df4283c3859719b6ecad45bbf41e2b1f4
fansi-sifan <sifan1121@gmail.com>
2019-09-25 17:47:03 -0400

    add school proficiency data

--- func.R
+++ func.R
@@ -6,13 +6,19 @@ library(tidyverse)
 
 key <- Sys.getenv("CENSUS_API_KEY")
 
+# df <- get_SBO(region = "metropolitan statistical area/micropolitan statistical area:24340")
+# df <- df %>%
+#   # filter(RACE_GROUP %in% race_code) %>%
+#   filter(NAICS2012 %in% traded_code) %>%
+#   filter(ETH_GROUP == "001" & SEX == "001")
+
 get_SBO <- function(...){
   getCensus(name = "2012/sbo",
             vars = c( "NAICS2012","NAICS2012_TTL",
                       "RACE_GROUP_TTL","RACE_GROUP", 
                       "ETH_GROUP","ETH_GROUP_TTL",
-                      "SEX","SEX_TTL", "GEO_ID",
-                      "FIRMALL", "FIRMPDEMP"),
+                      "SEX","SEX_TTL", "GEO_ID","GEO_TTL",
+                      "FIRMALL", "FIRMPDEMP", "FIRMPDEMP_S", "FIRMPDEMP_F", "FIRMPDEMP_S_F"),
             ...,
             key = key)
 }
@@ -28,6 +34,8 @@ create_labels <- function(df) {
 # DATASETS ==============================================
 # define geographies
 MetroDenver_cbsa <- c("22660", "19740", "14500", "24540")
+Denverpeer_cbsa <- c("12420", "33460", "38060","38900", "41740", "41620", "42660")
+GRpeer_cbsa <- c("10900","19820", "24860","33460","46140")
 
 # MetroDenvr_approx <- (county_cbsa_st %>%
 #                         filter(cbsa_code %in% MetroDenver_cbsa))$stco_code
@@ -45,27 +53,33 @@ GR_county <- (county_cbsa_st%>%filter(cbsa_code%in%GR_cbsa))$stco_code
 # 1. No job
 # out of work
 
-get_oow <- function(code) {
+get_oow <- function(metrocode, peercode) {
   load("../metro-dataset/out_of_work/co_oow.rda")
+  code <- c(metrocode, peercode)
 
   oow <- co_oow %>%
-    filter(cbsa_code %in% code)
+    filter(cbsa_code %in% metrocode)
 
-  df <- oow %>%
+  df <- co_oow %>%
+    filter(cbsa_code %in% code)%>%
     filter(population == "Sample population")
 
   oow_summary <- bind_cols(create_labels(co_oow), data.table::transpose(df))
   return(list(oow_summary, oow))
 }
 
-get_yoow <- function(code) {
+tmp <- get_oow(MetroDenver_cbsa, Denverpeer_cbsa)
+
+get_yoow <- function(metrocode, peercode) {
   # young out of work
   load("../metro-dataset/out_of_work/co_oow_young.rda")
-
+  code <- c(metrocode, peercode)
+  
   yoow <- co_oow_young %>%
-    filter(cbsa_code %in% code)
+    filter(cbsa_code %in% metrocode)
 
-  df <- yoow %>%
+  df <- co_oow_young %>%
+    filter(cbsa_code %in% code)%>%
     filter(population == "Out-of-work population")
 
   yoow_summary <- bind_cols(create_labels(co_oow_young), data.table::transpose(df))
@@ -75,28 +89,29 @@ get_yoow <- function(code) {
 
 # 2. Bad job
 # low wage
-get_lww <- function(code) {
+get_lww <- function(metrocode, peercode) {
   load("../metro-dataset/low_wage_worker/cbsa_low_wage_worker.rda")
-
+  code <- c(metrocode, peercode)
   lww <- cbsa_low_wage_worker %>%
-    filter(cbsa_code %in% code)
+    filter(cbsa_code %in% metrocode)
 
-  df <- lww %>%
+  df <- cbsa_low_wage_worker %>%
+    filter(cbsa_code %in% code)%>%
     filter(population == "Low-wage workers")
-
 
   lww_summary <- bind_cols(create_labels(cbsa_low_wage_worker), data.table::transpose(df))
   return(list(lww_summary,lww))
 }
 
 # opportunity industries
-get_opp <- function(code) {
+get_opp <- function(metrocode, peercode) {
   # BA/2-yr but no good job
   load("../metro-dataset/opportunity industries/cbsa_oppind_race.rda")
   load("../metro-dataset/opportunity industries/cbsa_oppind.rda")
-
+  code <- c(metrocode, peercode)
+  
   opp <- cbsa_oppind_race %>%
-    filter(cbsa_code %in% code)
+    filter(cbsa_code %in% metrocode)
 
   opp_summary <- cbsa_oppind %>%
     filter(cbsa_code %in% code) 
@@ -131,7 +146,7 @@ get_sbo_m <- function(stco_code){
   sbo_df <- purrr::map_df(stco_code, get_sbo)
   
   sbo_summary <- sbo_df %>%
-    group_by(state, county, RACE_GROUP) %>%
+    group_by(state, county, GEO_TTL, RACE_GROUP) %>%
     mutate(firmdemp = as.numeric(FIRMPDEMP)) %>%
     summarise(firmdemp = sum(firmdemp)) %>%
     spread(RACE_GROUP, firmdemp) %>%
@@ -143,15 +158,25 @@ get_sbo_m <- function(stco_code){
 }
 
 
-# 4. high school compeletion
+# 4. School proficiency 
+
+get_school <- function(code){
+  load("../metro-dataset/school proficiency/co_school_scores.rda")
+  
+  df <- co_school_scores %>%
+    filter(stco_code %in% code)
+  
+  return(list(df))
+}
 
 
 # 5. Flat income
 # housing
 
-get_hiratio <- function(code) {
-  load("../../metro_data_warehouse/data_final/housing_price/cbsa_housing_price.rda")
-
+get_hiratio <- function(metrocode, peercode) {
+  load("../metro-dataset/housing_price/cbsa_housing_price.rda")
+  code <- c(metrocode, peercode)
+  
   df <- cbsa_housing_price %>%
     filter(cbsa_code %in% code)
   
