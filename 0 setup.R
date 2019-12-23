4824da2f6eaec6fe4e7d74822f83f784749dd690
fansi-sifan <sifan1121@gmail.com>
2019-12-23 14:45:55 -0500

    test with GR data

--- 0 setup.R
+++ 0 setup.R
@@ -6,11 +6,13 @@ library(metro.data)
 # define geographies ==============================================
 
 # targets  ----------------------
-name <- "Metro Denver"
+# name <- "Metro Denver"
+name <- "Grand Rapids"
 # MetroDenver
-target_cbsa <- c("22660", "19740", "14500", "24540")
+# target_cbsa <- c("22660", "19740", "14500", "24540")
+target_cbsa_core <- target_cbsa <- "24340"
 # if multiple cbsa, select a core
-target_cbsa_core <- "19740"
+# target_cbsa_core <- "19740"
 # GR cbsa
 # target_cbsa <- "24340"
 
@@ -25,20 +27,18 @@ target_co <- (county_cbsa_st %>% filter(cbsa_code %in% target_cbsa))$stco_code
 # peers -------------------------
 
 # Denver peers
-peer_cbsa <- c("12420", "33460", "38060", "38900", "41740", "41620", "42660")
+# peer_cbsa <- c("12420", "33460", "38060", "38900", "41740", "41620", "42660")
 
 # Grand Rapids peers
-# peer_cbsa <- c("14260","17140","17460","24860","26900","33340","33460","34980","41620","46140","48620")
-
+peer_cbsa <- c("14260","17140","17460","24860","26900","33340","33460","34980","41620","46140","48620")
 
 peer_co <- (county_cbsa_st %>% filter(cbsa_code %in% peer_cbsa))$stco_code
 
 # communting zones
-peer_cz <- c("Denver", "Austin","Boulder","Fort Collins","Greeley","Minneapolis","Phoenix","Portland",
-                "Salt Lake City","San Diego","Seattle")
+# peer_cz <- c("Denver", "Austin","Boulder","Fort Collins","Greeley","Minneapolis","Phoenix","Portland","Salt Lake City","San Diego","Seattle")
 
-# peer_cz <- c("Grand Rapids","Wichita", "Tulsa","Greenville", "Salt Lake City",
-#                 "Cincinnati", "Nashville", "Boise City", "Cleveland","Milwaukee", "Indianapolis", "Minneapolis")
+peer_cz <- c("Grand Rapids","Wichita", "Tulsa","Greenville", "Salt Lake City",
+                "Cincinnati", "Nashville", "Boise City", "Cleveland","Milwaukee", "Indianapolis", "Minneapolis")
 
 # download datasets ------------------
 
