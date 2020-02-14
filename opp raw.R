08a64e6bdc2f1ff0ddc52e7c872d3d3a98b3554b
fansi-sifan <sifan1121@gmail.com>
2020-02-14 17:43:29 -0500

    Update opp raw.R

--- opp raw.R
+++ opp raw.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 
-# read in opportunity job mix
-# opp_path <- "V:/Performance/Project files/Opportunity Industries/Data/Output/Final/Metros/19740 Denver CO/19740 3digit flows.csv"
-opp_path <- "V:/Performance/Project files/Opportunity Industries/Data/Output/Final/Metros/24340 Grand Rapids MI/24340 Grand Rapids MI 3digit flows.csv"
+# read in opportunity job mix --------
 
-opp_raw <- read_csv(opp_path)
+# raw data 
+opp_path <- "V:/Performance/Project files/Opportunity Industries/Data/Output/Final/Metros/"
+# opp_path <- "V:/Performance/Project files/Opportunity Industries/Data/Output/Final/Metros/24340 Grand Rapids MI/24340 Grand Rapids MI 3digit flows.csv"
+
+opp_raw <- read_csv(paste0(opp_path,"19740 Denver CO/19740 3digit flows.csv"))
 
 skimr::skim(opp_raw)
 
@@ -13,19 +15,22 @@ skimr::skim(opp_raw)
 # summary(as.factor(opp_raw$education))
 # summary(as.factor(opp_raw$age))
 # summary(as.factor(opp_raw$titlea))
+summary(as.factor(opp_raw$occ_a))
 
+
+# wage by age and race
 opp_sp <- opp_raw %>%
   filter(age == "Total") %>%
   filter(gender == "Total") %>%
-  mutate(edu = ifelse(education %in% c("Associate's degree","Some college or certificate", "High school diploma"),"High School",education))%>%
-  group_by(race, edu) %>%
+  # mutate(edu = ifelse(education %in% c("Associate's degree","Some college or certificate", "High school diploma"),"High School",education))%>%
+  group_by(race, education) %>%
   summarise(p_a_wage = weighted.mean(p_a_wage, weight),
             f_a_wage = weighted.mean(f_a_wage, weight),
-            total = sum(weight))
+            total = sum(weight)) 
 
 skimr::skim(opp_sp)
 
-metro.data::bbplot(opp_sp, aes(x = edu, y = p_a_wage, fill = race))+
+metro.data::bbplot(opp_sp, aes(x = education, y = p_a_wage, fill = race))+
   geom_col(position = "dodge")
 
 load("../metro-dataset/acs5_2017/co_acs_raw.rda")
@@ -53,4 +58,6 @@ co_acs_raw %>%
   View()
 
 
+# read demo files =============
 
+demo_col <- readxl::read_excel(paste0(opp_path, "19740 Denver CO/19740 Denver CO BMPP Opportunity Industries - 2017 Job shares.xlsx"), sheet = "DEMO2_OFCOL")
